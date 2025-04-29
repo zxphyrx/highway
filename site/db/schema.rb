b3bfdb8bbeadcb2c0e0c504ddfc80cccfb50db11
acon <101356372+acornitum@users.noreply.github.com>
2025-04-29 12:11:32 -0400

    remove users database

--- site/db/schema.rb
+++ site/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_27_200919) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_27_015515) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,12 +54,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_27_200919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hours"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
