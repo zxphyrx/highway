a1f7ccc3af006bb206d219dbbc1ef8f2f54d7855
acon <101356372+acornitum@users.noreply.github.com>
2025-05-02 18:11:25 -0400

    add create new post button on sidebar when user is signed in

--- site/db/schema.rb
+++ site/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_29_161341) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_29_211703) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_29_161341) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hours"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -66,4 +68,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_29_161341) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "posts", "users"
 end
