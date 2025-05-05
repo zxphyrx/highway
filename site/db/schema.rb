6d2a1eaf6fe2e8ed093a32ace465b9264f4afd67
acon <101356372+acornitum@users.noreply.github.com>
2025-05-05 19:50:17 -0400

    update schema

--- site/db/schema.rb
+++ site/db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_05_200739) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hours"
-    t.integer "user_id", null: false
+    t.integer "user_id"
     t.integer "project_id", null: false
     t.index ["project_id"], name: "index_posts_on_project_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
