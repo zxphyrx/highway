b25aa9df956be3808add1eff2243894466c2d130
Max Wofford <max@maxwofford.com>
2025-05-07 23:52:55 -0400

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,9 @@ Rails.application.configure do
   # Turn on fragment caching in view templates.
   config.action_controller.perform_caching = true
 
+  # Don't require master key
+  config.require_master_key = false
+
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
