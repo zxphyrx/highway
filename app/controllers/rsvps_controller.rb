610abcdfd6feb6de79e7b0a0fba73a3877e63dcb
aqua <84078890+phthallo@users.noreply.github.com>
2025-05-12 15:27:34 -0400

    fix rsvps + background music

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -4,16 +4,14 @@ class RsvpsController < ApplicationController
     if @rsvp.airtable_record_id.blank?
       @rsvp.update!(url_params: rsvp_params[:url_params])
     end
-    flash[:success] = "Thanks for your interest! We'll be in touch soon."
-    redirect_to root_path
+    redirect_to root_path, flash: { notice: "Thanks for your interest! We'll be in touch soon." }
   rescue ActiveRecord::RecordInvalid
-    flash[:error] = "Please enter a valid email address."
-    redirect_to root_path
+    redirect_to root_path, flash: { alert: "Please enter a valid email address." }
   end
 
   private
 
   def rsvp_params
-    params.permit(:email, :url_params)
+    params.permit(:email, :url_params, :authenticity_token, :commit)
   end
 end
