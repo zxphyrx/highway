b8c08871aa6f07ee9082a9aa403d811463113384
Max Wofford <max@maxwofford.com>
2025-05-13 12:27:14 -0400

    Further configs for good_job

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,5 +1,10 @@
 Rails.application.configure do
   config.active_job.queue_adapter = :good_job
   config.good_job.preserve_job_records = true
-  config.good_job.max_threads = 1
+  config.good_job.cleanup_preserved_jobs_before_seconds_ago = 60 * 60 * 24 * 7
+  config.good_job.cleanup_interval_jobs = 1000
+  config.good_job.cleanup_interval_seconds = 3600
+
+  config.good_job.enable_cron = true
+  config.good_job.execution_mode = :async
 end
